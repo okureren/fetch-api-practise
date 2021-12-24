71c324282058906e23945c6ac887e5d5187b0877
Eren Okur <okuryasineren@gmail.com>
2021-12-24 18:56:55 +0300

    bug fixed

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const rowDOM = document.querySelector(".row");
-// rowDOM.setAttribute("class", "justify-content-center");
-
-// fetch("https://jsonplaceholder.typicode.com/photos")
-//   .then((response) => response.json())
-//   .then((photos) => {
-//     for (let i = 0; i < 21; i++) {
-//       let colDom = document.createElement("div");
-//       colDom.setAttribute("class", "col-6");
-//       colDom.innerHTML = photos[i].title;
-//       colDom.style.backgroundImage = "url(" + photos[i].url + ")";
-//       rowDOM.appendChild(colDom);
-//     }
-//   });
 
 fetch("https://jsonplaceholder.typicode.com/photos")
   .then((response) => response.json())
   .then((photos) => {
     for (let i = 0; i < 21; i++) {
       let colDom = document.createElement("div");
-      colDom.setAttribute("class", "col-8");
-      let titleDOM = document.createElement("h1");
-      titleDOM.setAttribute("class", "text-primary");
-      titleDOM.innerHTML = photos[i].title;
+      let colRowDOM = document.createElement("div");
+      let colTitle = document.createElement("div");
+      let colIMG = document.createElement("div");
+      let titleDOM = document.createElement("h5");
       let imgDOM = document.createElement("img");
+
+      colDom.setAttribute("class", "col-12 ");
+      colRowDOM.setAttribute("class", "row");
+      colTitle.setAttribute("class", "col-4");
+      colIMG.setAttribute("class", "col-4");
+      titleDOM.setAttribute("class", "text-primary");
       imgDOM.setAttribute("src", photos[i].thumbnailUrl);
+      imgDOM.setAttribute("class", "img-fluid");
+
+      titleDOM.innerHTML = `${photos[i].id}) ${photos[i].title}`;
+
       rowDOM.appendChild(colDom);
-      colDom.appendChild(titleDOM);
-      colDom.appendChild(imgDOM);
+      colDom.appendChild(colRowDOM);
+      colRowDOM.appendChild(colTitle);
+      colRowDOM.appendChild(colIMG);
+      colTitle.appendChild(titleDOM);
+      colIMG.appendChild(imgDOM);
     }
   });
